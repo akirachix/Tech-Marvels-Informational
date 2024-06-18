54f6830d45a76206bdb5ad7ad0ac1a7dd26fb410
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-06-18 09:03:51 +0300

    added products

---
+++ wezesha/src/Product/index.js
@@ -0,0 +1,53 @@
+import './index.css';
+import mobileapp from '../images/mobileapp.png';
+import dashboard from '../images/dashboard.png';
+
+const Product = () =>{
+    return(
+        <body>
+            <div id="product-heading">
+                <h1>Products</h1>
+                <p id="product-info">We will have a mobile app for farmers and a dashboard for Saccos and cooperatives.
+                    By providing a web app and dashboard that intergrates with cooperatives and Saccos. 
+                    Wezesha will give farmers complete access to their documents, including dairy milk
+                    productivity, deliveries, and sales transactions.
+                </p>
+            </div>
+            <div id= 'wezesha-mobileapp'>
+                <div>
+                    <img id= "mobilepic" src ={mobileapp} alt= "mobileapp-photo"/>
+                </div>
+                <div className='about-mobileapp'>
+                    <h1>Wezesha Mobile App</h1>
+                    <p>
+                        The mobile app will empower dairy
+                        farmers by providing them with a platform
+                        for keeping their dairy activities and 
+                        accessing information from cooperatives.
+                    </p>
+                </div>
+            </div>
+
+            <div id= 'wezesha-dashboard'>
+                <div className='about-dashboard'>
+                    <h1>Wezesha Dashboard</h1>
+                    <p>Wezesha dashboard will help SACCOs improve
+                        their loan evaluation processes, and streamline
+                        record-keeping contributing to their growth and
+                        sustainability.
+                    </p>
+                </div>
+                <div>
+                    <img id= "dashboard-pic" src ={dashboard} alt= "Wezesha dashboard"/>  
+                </div>
+            </div>
+
+
+
+
+        </body>
+    );
+};
+
+  
+export default Product;
