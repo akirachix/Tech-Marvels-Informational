4a8c92b73398e6161b691ea2a2b04f29d1ba2645
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-07-05 16:51:06 +0300

    made changes in about

--- wezesha/src/App.js
+++ wezesha/src/App.js
@@ -1,13 +1,11 @@
 import Product from "./Product"
-import {About} from './About'
+import About from './About'
 
 const wezeshaApp =()=>{
   return(
     <div>
-      <h1></h1>
       <About/>
       <Product/>
-      
     </div>
   )
 }
