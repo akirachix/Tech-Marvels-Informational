2afb9a890386a0781d110386c5b404a3c4b30c3d
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-07-10 07:03:01 +0300

    added files

---
+++ wezesha/wezesha/src/App.js
@@ -0,0 +1,15 @@
+import Landingpage  from "./Landing";
+import Navbar from "./Navbar";
+
+
+
+
+const App =()=>{
+  return(
+    <div>
+      <Navbar/>
+      <Landingpage/>
+    </div>
+  )
+}
+export default App;
--- wezesha/wezesha/src/Landing/index.js
+++ wezesha/wezesha/src/Landing/index.js
@@ -1,26 +1,13 @@
 
 import "./index.css";
 import farmer from '../images/farmer.jpg'
-import logo from '../images/logo.png'
+
 
 
 
 const Landingpage = () =>{
   return (
     <div className="landingpagecontent">
-        <div className="heading">
-        <img src={logo} alt="logo" className="logopicture"/>
-
-          <div className="navbar">
-            <a >Home</a>
-            <a>About Us</a>
-        
-      <a>Products</a>
-            <a>Our Team</a>
-            <a>Contact Us</a>
-          </div>
-          
-        </div>
         <div className="wezeshawelcometext">
         <div className="wezeshatext">
           <strong className="wezeshatexts">Wezesha</strong> is a robust digital software solution that caters to the needs
